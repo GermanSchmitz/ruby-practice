3d8ab0644161a3d8617f020fdbb9b8950dbab491
German Schmitz <germanschmitz123@gmail.com>
2023-08-15 13:09:45 -0300

    implement constant yes and move folder test to PasswordGenerator

--- PasswordGenerator/main.rb
+++ PasswordGenerator/main.rb
@@ -3,27 +3,29 @@ require_relative 'password_generator'
 
 password_generator = PasswordGenerator.new
 
+YES = 'y'
+
 loop do
   puts "\nPassword Generator"
   print "Enter desired password length: "
   length = gets.chomp.to_i
 
   print "Include lowercase characters? (y/n): "
-  use_lowercase = gets.chomp.downcase == 'y'
+  use_lowercase = gets.chomp.downcase == YES
 
   print "Include uppercase characters? (y/n): "
-  use_uppercase = gets.chomp.downcase == 'y'
+  use_uppercase = gets.chomp.downcase == YES
 
   print "Include digit characters? (y/n): "
-  use_digits = gets.chomp.downcase == 'y'
+  use_digits = gets.chomp.downcase == YES
 
   print "Include special characters? (y/n): "
-  use_special = gets.chomp.downcase == 'y'
+  use_special = gets.chomp.downcase == YES
 
   password = password_generator.generate_secure_password(length, use_lowercase, use_uppercase, use_digits, use_special)
   puts "\nGenerated Password: #{password}"
 
   print "Generate another password? (y/n): "
   continue = gets.chomp.downcase
-  break if continue != 'y'
+  break if continue != YES
 end
---
+++ PasswordGenerator/test/test_password_gen.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+require 'minitest/autorun'
+require_relative '../password_generator'
+
+class TestPasswordGen < Minitest::Test
+
+  def setup
+    @password_generator = PasswordGenerator.new
+  end
+
+  def test_password_only_lowercase
+    password = @password_generator.generate_secure_password(20, true, false, false, false)
+    assert_match(/[a-z]+/, password)
+  end
+
+  def test_generate_pass_uppercase
+    password = @password_generator.generate_secure_password(20, false, true, false, false)
+    assert_match(/[A-Z]+/, password)
+  end
+
+  def test_generate_password_digits
+    password = @password_generator.generate_secure_password(20, false, false, true, false)
+    assert_match(/\d+/, password)
+  end
+
+  def test_gen_pass_specialchars
+    password = @password_generator.generate_secure_password(20, false, false, false, true)
+    assert_match(/[\W_]+/, password)
+  end
+
+  def test_generate_pass_security
+    50.times do
+      password = @password_generator.generate_secure_password(20, true, true, true, true)
+      assert_match(/[a-zA-Z\d\W_]+/, password)
+      assert_match(/[a-z]+/, password)
+      assert_match(/[A-Z]+/, password)
+      assert_match(/\d+/, password)
+      assert_match(/[\W_]+/, password)
+    end
+  end
+end
